2d872e53528df24a046e3b05198f8f6949581a09
cijiugechu <cijiugechu@gmail.com>
2024-08-18 20:13:19 +0800

    chore: 迁移至 pnpm workspace

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CssBaseline from '@mui/material/CssBaseline';
 import { App } from './App';
 import { Home } from './home/Home';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -22,8 +21,3 @@ root.render(
         </BrowserRouter>
     </React.Fragment>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
