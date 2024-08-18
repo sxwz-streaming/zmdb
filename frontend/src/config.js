cf3e804b65f0683266164f141209c9fe12f1cf7d
cijiugechu <cijiugechu@gmail.com>
2024-08-18 20:46:36 +0800

    feat: 修改 index.html 以自由定义 endpoint, 摆脱 config.js 依赖

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,9 +1,9 @@
-const port = process.env.BACKEND_PORT || 10888;
-const endPoint = process.env.API_ENDPOINT || 'localhost';
+// const port = process.env.BACKEND_PORT || 10888;
+// const endPoint = process.env.API_ENDPOINT || 'localhost';
 
 export default {
   url: {
-      api: `//${endPoint}:${port}`,
+      api: `//${window.__apiEndpoint__}`,
       author: '//space.bilibili.com',
       clip: '//www.bilibili.com/video'
   }
