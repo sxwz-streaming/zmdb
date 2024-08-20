3289abb3df14ea08daa3379cc5c97d17f619abbd
wryx166 <wryx166@outlook.com>
2024-08-21 01:29:12 +0800

    bugfix 详细： 由于文件./data.db并不存在，docker-compose挂载时将其当做文件夹创建， 进而导致后端无法创建同名文件，抛出SqliteError: unable to open database file 后端无法正常启动

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,10 @@
+// 引入 path 模块  
+import path from 'node:path';
+
 const port = process.env.BACKEND_PORT || 10888;
 const tmpDir = process.env.BACKEND_TMP_DIR || './tmp';
-const dbPath = process.env.BACKEND_DB_PATH || './data.db';
+const dbFolderPath = process.env.BACKEND_DB_PATH || './data';
+const dbPath = path.join(dbFolderPath, 'db.sqlite');
 const bodyLimit = process.env.BACKEND_BODY_LIMIT || '10mb';
 
 export default {
